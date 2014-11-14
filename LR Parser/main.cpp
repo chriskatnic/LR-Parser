f9d02a37003e87584ac52c529ac4763a4d0eaba3
Christopher Katnic <Chris@Christophers-MacBook-Pro.local>
2014-11-13 20:15:38 -0800

    Initial Commit

---
+++ LR Parser/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  LR Parser
+//
+//  Created by Christopher Katnic on 11/13/14.
+//  Copyright (c) 2014 Christopher Katnic. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
